5c22a1b7a59c26446d699a07203b221d08fe3549
eogns47 <eogns47@naver.com>
2022-11-28 14:57:26 +0900

    KangManJoo 22/11/28 2467 submit

--- 22_November/28/2467_KangManJoo.cpp
+++ 22_November/28/2467_KangManJoo.cpp
@@ -6,9 +6,9 @@ int arr[2][100001];           //first index ==0 ->negative , index==1 ->positive
 int p = 0;
 int n = 0;
 int liqA, liqB;
-                              // 자신 그룹의 앞뒤와 반대 그룹에서의 binary search 후 앞뒤 총 4개를 비교한다.
-int binary_search(int pos[],int x) {
-	int left = 0; 
+// 자신 그룹의 앞뒤와 반대 그룹에서의 binary search 후 앞뒤 총 4개를 비교한다.
+int binary_search(int pos[], int x) {
+	int left = 0;
 	int right = p;
 	int mid = 0;
 	while (left < right) {
@@ -41,13 +41,7 @@ int main() {
 	}
 	int min = 2000000000;
 	for (int i = 0; i < n; i++) {
-		if(i!= n-1)        //i != last index
-			if (min > abs(arr[0][i] + arr[0][i + 1])) {
-				min = abs(arr[0][i] + arr[0][i + 1]);
-				liqA = arr[0][i];
-				liqB = arr[0][i + 1];
-			}
-		int idx=binary_search(arr[1],arr[0][i]*(- 1));
+		int idx = binary_search(arr[1], arr[0][i] * (-1));
 		if (idx != p) {
 			if (min > abs(arr[1][idx] + arr[0][i])) {
 				min = abs(arr[1][idx] + arr[0][i]);
@@ -56,22 +50,26 @@ int main() {
 			}
 		}
 		if (idx != 0) {
-			if (min > abs(arr[1][idx-1] + arr[0][i])) {
-				min = abs(arr[1][idx-1] + arr[0][i]);
+			if (min > abs(arr[1][idx - 1] + arr[0][i])) {
+				min = abs(arr[1][idx - 1] + arr[0][i]);
 				liqA = arr[0][i];
-				liqB = arr[1][idx-1];
+				liqB = arr[1][idx - 1];
 			}
 		}
 
 	}
-	for (int i = 0; i < p; i++) {
-		if (i != p - 1)        //i != last index
-			if (min > arr[1][i] + arr[1][i + 1]) {
-				min = arr[1][i] + arr[1][i + 1];
-				liqA = arr[1][i];
-				liqB = arr[1][i + 1];
-			}
-			
+	if (n >= 2) {
+		if (min > abs(arr[0][n - 1] + arr[0][n - 2])) {
+			min = abs(arr[0][n - 1] + arr[0][n - 2]);
+			liqA = arr[0][n - 2];
+			liqB = arr[0][n - 1];
+		}
+	}
+	if (p >= 2) {
+		if (min > arr[1][0] + arr[1][1]) {
+			liqA = arr[1][0];
+			liqB = arr[1][1];
+		}
 	}
 	cout << liqA << " " << liqB;
 
