8144343caae7848f35f5b2d2495daae1bf7cd6f3
KwanHee Lee <kwan7595@naver.com>
2022-12-15 19:24:49 +0900

    kwan 22/12/15 1043 submit

--- 22_December/15/1043_kwan.py
+++ 22_December/15/1043_kwan.py
@@ -1,7 +1,6 @@
 n,m=map(int,input().split())
 ans = list(map(int,input().split()))
 parent = [i for i in range(0,n+1)]
-rank = [0 for _ in range(n+1)]
 def find(x):
     if parent[x] != x:
         parent[x] = find(parent[x])
