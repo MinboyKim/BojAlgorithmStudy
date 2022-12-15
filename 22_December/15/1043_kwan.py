d0cb9f65840b8c275e8e8385b2c87c3a20dba97f
KwanHee Lee <kwan7595@naver.com>
2022-12-15 19:22:20 +0900

    kwan 22/12/14 12015 submit

--- 22_December/15/1043_kwan.py
+++ 22_December/15/1043_kwan.py
@@ -1,30 +1,41 @@
 n,m=map(int,input().split())
-t = input()
-llist = [set([i])for i in range(0,n+1)]
-liar = [False for _ in range(0,n+1)]
+ans = list(map(int,input().split()))
+parent = [i for i in range(0,n+1)]
+rank = [0 for _ in range(n+1)]
+def find(x):
+    if parent[x] != x:
+        parent[x] = find(parent[x])
+    return parent[x]
+def union(x,y):
+    x_root = find(x)
+    y_root = find(y)
+    if x_root in ans and y_root in ans:
+        return
+    if x_root in ans:
+        parent[y_root]=x_root
+    elif y_root in ans:
+        parent[x_root]=y_root
+    else:
+        if x_root<y_root:
+            parent[y_root] = x_root
+        else:
+            parent[x_root] = y_root
 parties = []
 cnt=0
-if t:
-    ans = set(list(map(int,t.split()))[1:])
+if ans[0]!=0:
+    ans = set(ans[1:])
     for _ in range(m):
         temp=list(map(int,input().split()))
-        party = set(temp[1:])
+        party = temp[1:]
+        for i in range(0,len(party)-1):
+            for j in range(i+1,len(party)):
+                union(party[i],party[j])
         parties.append(party)
-        if not ans.isdisjoint(party):
-            ans = ans.union(party)
-        for person in party:
-            llist[person] = llist[person].union(party)
-    for party in parties:
-        if not ans.isdisjoint(party):
-            ans = ans.union(party)
-    for i, num in enumerate(llist):
-        if not num.isdisjoint(ans):
-            liar[i] = True
     for party in parties:
         flag = True
-        for num in party:
-            if liar[num]:
-                flag=False
+        for person in party:
+            if find(person) in ans:
+                flag = False
                 break
         if flag:
             cnt+=1
---
+++ 22_December/15/README.md
@@ -0,0 +1,2 @@
+# Kwan
+## [1043 - 거짓말](https://www.acmicpc.net/problem/1043)
