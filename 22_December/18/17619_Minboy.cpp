b334e26049da72be654ba693c9364b5c61a63b43
Minboy <35567292+MinboyKim@users.noreply.github.com>
2022-12-18 19:56:30 +0900

    Update 17619_Minboy.cpp

--- 22_December/18/17619_Minboy.cpp
+++ 22_December/18/17619_Minboy.cpp
@@ -1,5 +1,5 @@
 #include <algorithm>
-#include <cstdio>
+#include <iostream>
 #include <tuple>
 
 using namespace std;
@@ -8,39 +8,43 @@ int N, Q;
 bool compare(tuple<int, int, int> a, tuple<int, int, int> b) {
   return get<0>(a) < get<0>(b);
 }
-tuple<int, int, int> bar[100'001];
-int group[100'001];
+tuple<int, int, int> bar[101'010];
+int group[101'010];
 
 int main(void) {
-  scanf("%d%d", &N, &Q);
+  ios::sync_with_stdio(false);
+  cin.tie(NULL);
+
+  int a, b, c;
+  cin >> N >> Q;
   for (int i = 0; i < N; i++) {
-    int a, b, c;
-    scanf("%d%d%d", &a, &b, &c);
+    cin >> a >> b >> c;
     bar[i] = {a, b, i};
   }
 
   sort(bar, bar + N, compare);
 
-  int sid = 0, e = get<1>(bar[0]);
-  group[0] = sid;
+  int num = 0;
+  int base = get<1>(bar[0]);
+  group[0] = num;
 
   for (int i = 1; i < N; i++) {
-    if (get<0>(bar[i]) <= e) {
-      if (get<1>(bar[i]) > e) e = get<1>(bar[i]);
+    if (get<0>(bar[i]) <= base) {
+      if (get<1>(bar[i]) > base) base = get<1>(bar[i]);
     } else {
-      sid++;
-      e = get<1>(bar[i]);
+      num++;
+      base = get<1>(bar[i]);
     }
-    group[get<2>(bar[i])] = sid;
+    group[get<2>(bar[i])] = num;
   }
   while (Q--) {
-    int u, v;
-    scanf("%d%d", &u, &v);
-    u--, v--;
-    if (group[u] == group[v])
-      puts("1");
+    cin >> a >> b;
+    a--;
+    b--;
+    if (group[a] == group[b])
+      cout << 1 << "\n";
     else
-      puts("0");
+      cout << 0 << "\n";
   }
 
   return 0;
