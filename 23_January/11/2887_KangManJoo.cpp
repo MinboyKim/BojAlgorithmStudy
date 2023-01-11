419acc94198c83325025de49e9ed2c36992437fe
eogns47 <eogns47@naver.com>
2023-01-11 19:34:58 +0900

    KangManJoo 23/1/11 2887,21738 submit

---
+++ 23_January/11/2887_KangManJoo.cpp
@@ -0,0 +1,22 @@
+//#include <bits/stdc++.h>
+//
+//using namespace std;
+//
+// void solve(){
+// 
+//	return;
+// }
+// 
+//void init() {
+//
+//	solve();
+//	return;
+//}
+//
+//int main() {
+//	 ios::sync_with_stdio(false);
+//	 cin.tie(NULL);
+//
+//	 init();
+//	 return 0;
+//}
---
+++ 23_January/11/README.md
@@ -0,0 +1,4 @@
+# KangManJoo
+## [2887번 - 행성 터널 PLATINUM V](https://www.acmicpc.net/problem/2887)
+## [21738번 - 얼음깨기 펭귄 GOLD V](https://www.acmicpc.net/problem/21738)
+
